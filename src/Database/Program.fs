369752f1ad632ab8fa7fc3bce27a66913c9fb7f8
carbon_twelve <carbon.tetrachloride@gmail.com>
2018-09-14 02:48:25 +0900

    Added declaration of PagedFile module interfaces

--- src/Database/Program.fs
+++ src/Database/Program.fs
@@ -3,6 +3,8 @@ open System.IO
 open System.Runtime.InteropServices
 open Microsoft.Win32.SafeHandles
 
+let undefined() = System.NotImplementedException() |> raise
+
 module Interop =
     [<DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)>]
     extern SafeFileHandle CreateFile(
@@ -15,6 +17,33 @@ module Interop =
         IntPtr templateFile
     )
 
+module PagedFile =
+    type IPageNum = interface end
+    
+    type IPFPageHandle =
+        abstract member GetData: unit -> byte[]
+        abstract member GetPageNum: unit -> IPageNum
+    
+    type IPFFileHandle =
+        abstract member GetFirstPage: unit -> IPFPageHandle
+        abstract member GetLastPage: unit -> IPFPageHandle
+        abstract member GetNextPage: current: IPageNum -> IPFPageHandle
+        abstract member GetPrevPage: current: IPageNum -> IPFPageHandle
+        abstract member GetThisPage: current: IPageNum -> IPFPageHandle
+        abstract member AllocatePage: unit -> IPFPageHandle
+        abstract member DisposePage: pageNum: IPageNum -> unit
+        abstract member MarkDarty: pageNum: IPageNum -> unit
+        abstract member UnpinPage: pageNum: IPageNum -> unit
+        abstract member ForcePages: pageNum: IPageNum -> unit
+
+    type IPFManager =
+        abstract member CreateFile: fileName: string -> unit
+        abstract member DestroyFile: fileName: string -> unit
+        abstract member OpenFile: fileName: string -> IPFFileHandle
+        abstract member CloseFile: fileHandle: IPFFileHandle -> unit
+        abstract member AllocateBlock: buffer: byte[] -> unit
+        abstract member DisposeBlock: buffer: byte[] -> unit
+                                                    
 [<EntryPoint>]
 let main argv = 
     let safeFileHandle =
